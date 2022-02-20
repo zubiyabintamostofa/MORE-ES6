beedcf2a12c10d2e9acaad904f1977add29c631e
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-20 23:27:05 +0600

    new

---
+++ destructuring.js
@@ -0,0 +1,10 @@
+const company={
+    name:'GP',
+    ceo:{id:1,name:'ajmol',food:'fuska'},
+    web:{work:'website development',employee:22,framework:'react'}
+
+
+};
+const {work,framework}=company. web;
+const {food}=company.ceo;
+console.log(work,framework,food);
