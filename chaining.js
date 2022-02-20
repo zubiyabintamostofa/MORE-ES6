cab6c6a418e0486595b745a7aef2481bfee1aba6
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-02-21 00:20:32 +0600

    two

---
+++ chaining.js
@@ -0,0 +1,12 @@
+//declear variable based on the name of an object property
+const myObject={
+    x:2,y:50,c:6,b:9
+}
+const{x,b}=myObject;
+//console.log(b);
+
+
+//destructuring array
+const[q,p]=[80,22];
+console.log(p);
+//undefine kichu khuje pete hole ? dile ta dea bujha jbe je seita undifine
